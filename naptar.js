3f1f80706301128fa0fd5b1d8b41c796409aa2e3
mateszadam <matesz.adam@gmail.com>
2022-09-24 16:11:11 +0200

    MA navbar kész

--- naptar.js
+++ naptar.js
@@ -4,3 +4,23 @@ window.onscroll = function () {
     let progressHeight = (window.pageYOffset / TotalHeight) * 100;
     progress.style.height = progressHeight + '%';
 }
+function delayki(){
+    setTimeout(navki, 300);
+}
+
+function navki(){
+    t1 = document.getElementById("title1").style.display = "block";
+    t2 = document.getElementById("title2").style.display = "block";
+    t3 = document.getElementById("title3").style.display = "block";
+}
+function navbe(){
+    t1 = document.getElementById("title1").style.display = "none";
+    t2 = document.getElementById("title2").style.display = "none";
+    t3 = document.getElementById("title3").style.display = "none";
+    setTimeout(delaybe, 300);
+}
+function delaybe(){
+    t1 = document.getElementById("title1").style.display = "none";
+    t2 = document.getElementById("title2").style.display = "none";
+    t3 = document.getElementById("title3").style.display = "none";
+}
