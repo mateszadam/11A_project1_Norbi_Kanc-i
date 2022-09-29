4b6fba881cb384e430855e6f4cb0b1e4c4c4c12b
mateszadam <matesz.adam@gmail.com>
2022-09-29 18:44:11 +0200

    MA összes pálya feltöltése

--- naptar.js
+++ naptar.js
@@ -1,35 +1,37 @@
-
 window.onscroll = function () {
-    let progress = document.getElementById('prograssbar');
-    let TotalHeight = document.body.scrollHeight - window.innerHeight;
-    let progressHeight = (window.pageYOffset / TotalHeight) * 100;
-    progress.style.height = progressHeight + '%';
-    fixheight();
+  let progress = document.getElementById("prograssbar");
+  let TotalHeight = document.body.scrollHeight - window.innerHeight;
+  let progressHeight = (window.pageYOffset / TotalHeight) * 100;
+  progress.style.height = progressHeight + "%";
+  fixheight();
+};
+
+function fixheight() {
+  height = document.getElementById("def_height").offsetHeight;
+  document.getElementById("height").style.height = height - 20 + "px";
+  document.getElementById("height2").style.height = height - 20 + "px";
+  document.getElementById("height3").style.height = height - 20 + "px";
+  document.getElementById("height4").style.height = height - 20 + "px";
+  document.getElementById("height5").style.height = height - 20 + "px";
+
 }
-function delayki(){
-    setTimeout(navki, 300);
+function delayki() {
+  setTimeout(navki, 300);
 }
 
-function fixheight(){
-    height = document.getElementById('def_height').offsetHeight;
-    console.log(height);
-    document.getElementById('height').style.height = height - 20 + "px";
+function navki() {
+  document.getElementById("title1").style.display = "block";
+  document.getElementById("title2").style.display = "block";
+  document.getElementById("title3").style.display = "block";
 }
-
-
-function navki(){
-    document.getElementById("title1").style.display = "block";
-    document.getElementById("title2").style.display = "block";
-    document.getElementById("title3").style.display = "block";
+function navbe() {
+  document.getElementById("title1").style.display = "none";
+  document.getElementById("title2").style.display = "none";
+  document.getElementById("title3").style.display = "none";
+  setTimeout(delaybe, 300);
 }
-function navbe(){
-    document.getElementById("title1").style.display = "none";
-    document.getElementById("title2").style.display = "none";
-    document.getElementById("title3").style.display = "none";
-    setTimeout(delaybe, 300);
-}
-function delaybe(){
-    document.getElementById("title1").style.display = "none";
-    document.getElementById("title2").style.display = "none";
-    document.getElementById("title3").style.display = "none";
+function delaybe() {
+  document.getElementById("title1").style.display = "none";
+  document.getElementById("title2").style.display = "none";
+  document.getElementById("title3").style.display = "none";
 }
